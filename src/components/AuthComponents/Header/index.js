955c03e189182688be8b27d702b40bd722e18d83
Pavel Chebotayev <pavel.chebotayev@gmail.com>
2021-06-16 23:37:14 +0300

    add navigation

--- src/components/AuthComponents/Header/index.js
+++ src/components/AuthComponents/Header/index.js
@@ -5,16 +5,13 @@ import styles from './styles.module.scss';
 import Logo from '../../../assets/images/logo.png';
 
 const Header = () => (
-  <div className={styles.headerWrapper}>
-    <div className={styles.logoContainer}>
-      <img src={Logo} alt="logo" />
-      <p>
-        Free republic of
-        <br />
-        <span>Liberland</span>
-      </p>
-    </div>
-    <div />
+  <div className={styles.logoContainer}>
+    <img src={Logo} alt="logo" />
+    <p>
+      Free republic of
+      <br />
+      <span>Liberland</span>
+    </p>
   </div>
 );
 
---
+++ src/components/Home/HomeHeader/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import { authActions } from '../../../redux/actions';
+import router from '../../../router';
+import Header from '../../AuthComponents/Header';
+
+import styles from './styles.module.scss';
+
+const HomeHeader = () => {
+  const titles = {
+    [router.home.profile]: 'My profile',
+    [router.home.documents]: 'My documents',
+    [router.home.feed]: 'Hi, {Username}!',
+    [router.home.constitution]: 'Constitution',
+    [router.home.voting]: 'Voting',
+    [router.home.wallet]: 'Wallet',
+  };
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+
+  const handleLogout = () => {
+    dispatch(authActions.signOut.call(history));
+  };
+
+  return (
+    <div className={styles.homeHeaderWrapper}>
+      <div className={styles.logoHeaderWrapper}>
+        <Header />
+      </div>
+      <div className={styles.homeHeaderAccountWrapper}>
+        <p>{titles[location.pathname]}</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeHeader;
