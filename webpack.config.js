afeb8577b748f89c3043ba3b97d75aea4f8ed4fd
Kacper Żuk <kacper.zuk@neti-soft.com>
2023-01-09 11:10:23 +0100

    Fix sourcemaps for dev

--- webpack.config.js
+++ webpack.config.js
@@ -44,108 +44,112 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
   return loaders;
 };
 
-module.exports = {
-  context: __dirname,
-  entry: ['babel-polyfill', './src/index.js'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '/',
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        resolve: {
-          fullySpecified: false,
-        },
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              [
-                require.resolve('babel-plugin-named-asset-import'),
-                {
-                  loaderMap: {
-                    svg: {
-                      ReactComponent:
-                        '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+module.exports = (env, argv) => {
+  return {
+    context: __dirname,
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      publicPath: '/',
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          resolve: {
+            fullySpecified: false,
+          },
+          use: {
+            loader: 'babel-loader',
+            options: {
+              include: [path.resolve(__dirname, "src")],
+              plugins: [
+                [
+                  require.resolve('babel-plugin-named-asset-import'),
+                  {
+                    loaderMap: {
+                      svg: {
+                        ReactComponent:
+                          '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                      },
                     },
                   },
-                },
+                ],
               ],
-            ],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|j?g|svg|gif)?$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: sassRegex,
-        exclude: sassModuleRegex,
-        use: getStyleLoaders(
-          {
-            importLoaders: 2,
-          },
-          'sass-loader',
-        ),
-        // Don't consider CSS imports dead code even if the
-        // containing package claims to have no side effects.
-        // Remove this when webpack adds a warning or an error for this.
-        // See https://github.com/webpack/webpack/issues/6571
-        sideEffects: true,
-      },
-      // Adds support for CSS Modules, but using SASS
-      // using the extension .module.scss or .module.sass
-      {
-        test: sassModuleRegex,
-        use: getStyleLoaders(
-          {
-            importLoaders: 2,
-            modules: true,
-          },
-          'sass-loader',
-        ),
-      },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|j?g|svg|gif)?$/,
+          use: 'file-loader',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: sassRegex,
+          exclude: sassModuleRegex,
+          use: getStyleLoaders(
+            {
+              importLoaders: 2,
+            },
+            'sass-loader',
+          ),
+          // Don't consider CSS imports dead code even if the
+          // containing package claims to have no side effects.
+          // Remove this when webpack adds a warning or an error for this.
+          // See https://github.com/webpack/webpack/issues/6571
+          sideEffects: true,
+        },
+        // Adds support for CSS Modules, but using SASS
+        // using the extension .module.scss or .module.sass
+        {
+          test: sassModuleRegex,
+          use: getStyleLoaders(
+            {
+              importLoaders: 2,
+              modules: true,
+            },
+            'sass-loader',
+          ),
+        },
+      ],
+    },
+    devtool: argv.mode === 'development' ? 'eval-source-map' : undefined,
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: 'index.html',
+      }),
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+      new Dotenv()
+      // new InterpolateHtmlPlugin({PUBLIC_URL: 'static' }),
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-      filename: 'index.html',
-    }),
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-    new Dotenv()
-    // new InterpolateHtmlPlugin({PUBLIC_URL: 'static' }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.js'],
-    fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer/'),
+    resolve: {
+      extensions: ['.ts', '.js'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+      },
+      alias: {
+        buffer: 'buffer',
+        stream: 'stream-browserify',
+      },
     },
-    alias: {
-      buffer: 'buffer',
-      stream: 'stream-browserify',
-    },
-  },
+  };
 };
