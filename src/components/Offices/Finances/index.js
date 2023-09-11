49a55814a5da797e1b4312beaae6d9a62074fb6f
Michal Lustyk <MichalLustykNeti>
2023-09-11 12:09:51 +0200

    added Council addres

--- src/components/Offices/Finances/index.js
+++ src/components/Offices/Finances/index.js
@@ -23,6 +23,10 @@ const ACCOUNTS = [
     name: "Treasury",
     address: "5EYCAe5ijiYfyeZ2JJCGq56LmPyNRAKzpG4QkoQkkQNB5e6Z",
   },
+  {
+    name: "Council",
+    address: "5EYCAe5g8CDuMsTief7QBxfvzDFEfws6ueXTUhsbx5V81nGH",
+  },
   // FIXME 1 LLD faucet
 ];
 
