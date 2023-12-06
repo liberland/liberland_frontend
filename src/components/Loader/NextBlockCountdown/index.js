da79a32e9d97b8332fa81421190c7d7515ad9547
Michal Lustyk <MichalLustykNeti>
2023-12-06 20:35:11 +0100

    BLOCKCHAIN-299 swapped count down to progress bar

--- src/components/Loader/NextBlockCountdown/index.js
+++ src/components/Loader/NextBlockCountdown/index.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
 
+import { BN, BN_ZERO } from '@polkadot/util';
 import { blockchainSelectors } from '../../../redux/selectors';
+import styles from './styles.module.scss';
 
 export default function NextBlockCountdown() {
   const currentBlockTimestamp = useSelector(blockchainSelectors.blockTimestamp);
@@ -19,11 +21,17 @@ export default function NextBlockCountdown() {
     return () => clearInterval(timer);
   }, [nextBlockCounter, currentBlockTimestamp]);
 
+  const progress = new BN((Math.trunc(nextBlockCounter / 100) / 60) * 100);
+  const progressBarRatio = new BN(progress).gt(BN_ZERO)
+    ? `${progress.toString()}%`
+    : '0%';
+
   return (
-    <>
-      Next block in:
-      {' '}
-      {Math.trunc(nextBlockCounter / 100) / 10}
-    </>
+    <div className={styles.progressWrapper}>
+      Eta:
+      <div className={styles.progressBar}>
+        <div className={styles.yayProgressBar} style={{ width: progressBarRatio }} />
+      </div>
+    </div>
   );
 }
