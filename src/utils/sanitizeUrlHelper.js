f61738e28a1971f765388bba77d3c5e9b2ffd37c
Szymon.Zieba <szymon.zieba@neti-soft.com>
2024-01-02 12:45:55 +0100

    fix check if url start with http

--- src/utils/sanitizeUrlHelper.js
+++ src/utils/sanitizeUrlHelper.js
@@ -2,7 +2,7 @@ import { sanitizeUrl } from '@braintree/sanitize-url';
 
 const sanitizeUrlHelper = (url) => {
   const sanitizedUrl = sanitizeUrl(url);
-  const checkerStartHttp = ['http', 'https'].some((element) => sanitizedUrl.includes(element));
+  const checkerStartHttp = ['http://', 'https://'].some((element) => sanitizedUrl.startsWith(element));
   return !checkerStartHttp ? `http://${sanitizedUrl}` : sanitizedUrl;
 };
 
