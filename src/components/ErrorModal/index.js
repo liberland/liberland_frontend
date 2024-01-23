5c94984ce40261feaab1eb6b0425de9c45125cbf
Szymon.Zieba <szymon.zieba@neti-soft.com>
2024-01-23 14:23:01 +0100

    BLOCKCHAIN-337 FE Error screen should show on center of screen

--- src/components/ErrorModal/index.js
+++ src/components/ErrorModal/index.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { blockchainSelectors } from '../../redux/selectors';
 import Button from '../Button/Button';
 import { blockchainActions } from '../../redux/actions';
+import styles from './styles.module.scss';
 
 function ErrorModal({ children }) {
   const errorExistsAndUnacknowledged = useSelector(blockchainSelectors.errorExistsAndUnacknowledgedByUser);
@@ -12,45 +13,14 @@ function ErrorModal({ children }) {
   const dispatch = useDispatch();
 
   return (
-    <div style={{ position: 'relative' }}>
+    <div className={styles.content} style={{ position: 'relative' }}>
       { errorExistsAndUnacknowledged
     && (
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          zIndex: 9999999,
-          background: 'rgba(211,211,211,0.6)',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <div
-          style={{
-            width: '250px',
-            height: '250px',
-            backgroundColor: '#FFF',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            borderRadius: 10,
-            padding: '25px',
-            paddingBottom: '10px',
-          }}
-        >
-          <p style={{
-            fontWeight: '1.5rem',
-            marginBottom: '2.5rem',
-          }}
-          >
+      <div className={styles.modalWrapper}>
+        <div className={styles.modal}>
+          <p className={styles.text}>
             {blockchainError?.details}
           </p>
-          <br />
           <Button
             primary
             onClick={() => {
