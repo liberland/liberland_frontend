52b1af977e893114c7b173f08329e753fcdfcc08
Szymon.Zieba <szymon.zieba@neti-soft.com>
2024-07-08 14:29:57 +0200

    fix my preference order Votes

--- src/api/nodeRpcCall.js
+++ src/api/nodeRpcCall.js
@@ -858,8 +858,8 @@ const getCongressMembersWithIdentity = async (walletAddress) => {
 
   const councilMembersList = councilMembers.map((member) => member.toString());
   const candidatesList = candidates.map((candidate) => candidate[0].toString());
-  const currentCandidateVotesByUser = currentCandidateVotesByUserQuery.isSome
-    ? currentCandidateVotesByUserQuery.unwrap().votes.map((vote) => vote.toString())
+  const currentCandidateVotesByUser = !currentCandidateVotesByUserQuery.isEmpty
+    ? currentCandidateVotesByUserQuery.votes.map((vote) => vote.toString())
     : [];
   const runnersUpList = runnersUp.map(([who]) => who[1].toString());
 
