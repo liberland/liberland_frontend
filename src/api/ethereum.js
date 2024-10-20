d8cf47f4c9b0c839beb068934d6d1dcce71cf18f
jindra12.underdark <jindra12.underdark@gmail.com>
2024-10-20 23:35:13 +0200

    BLOCKCHAIN-522 Start refactoring existing code to add wallet selection

--- src/api/ethereum.js
+++ src/api/ethereum.js
@@ -1,46 +1,34 @@
-import { createThirdwebClient, getContract, readContract } from "thirdweb";
-import { defineChain } from "thirdweb/chains";
+import { createThirdwebClient, getContract, readContract } from 'thirdweb';
+import { defineChain } from 'thirdweb/chains';
 
-let thirdWebContractCache = null
-const getThirdWebContract = () => {
-  if (thirdWebContractCache === null) {
-    // create the client with your clientId, or secretKey if in a server environment
-    const client = createThirdwebClient({
-      clientId: "eaa651485162e88db620fcb0ac25cbbb"
-    });
-    // connect to your contract
-    const contract = getContract({
-      client,
-      chain: defineChain(1),
-      address: "0x286477f868Aa66C534254a16a7D532C7615Ac332"
-    });
-    thirdWebContractCache = contract
-    return contract
-  } else {
-    return thirdWebContractCache
-  }
-}
+const getThirdWebContract = (clientId, chainId, contractAddress) => {
+  // create the client with your clientId, or secretKey if in a server environment
+  const client = createThirdwebClient({
+    clientId,
+  });
+  // connect to your contract
+  const contract = getContract({
+    client,
+    chain: defineChain(chainId),
+    address: contractAddress,
+  });
+  return contract;
+};
 
-const getTokenAddress = async () => {
-  const contract = getThirdWebContract()
-  return readContract({
-    contract,
-    method: "function rewardToken() view returns (address)",
-    params: []
-  })
-}
+const getTokenAddress = async (contract) => readContract({
+  contract,
+  method: 'function rewardToken() view returns (address)',
+  params: [],
+});
 
-const getLPStakeInfo = async (userEthAddress) => {
-  const contract = getThirdWebContract()
-  return readContract({
-    contract,
-    method: "function getStakeInfo(address userEthAddress) view returns (uint256 _tokensStaked, uint256 _rewards)",
-    params: [userEthAddress]
-  })
-
-}
+const getLPStakeInfo = async (contract, userEthAddress) => readContract({
+  contract,
+  method: 'function getStakeInfo(address userEthAddress) view returns (uint256 _tokensStaked, uint256 _rewards)',
+  params: [userEthAddress],
+});
 
 export {
+  getThirdWebContract,
   getTokenAddress,
-  getLPStakeInfo
-}
+  getLPStakeInfo,
+};
