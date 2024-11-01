8909b7dd6f62cf5a225ca2b90a2d02524970dca1
jindra12.underdark <jindra12.underdark@gmail.com>
2024-11-01 23:10:51 +0100

    BLOCKCHAIN-523 Style proposal table correctly

--- src/components/Proposal/ProposalTable/index.js
+++ src/components/Proposal/ProposalTable/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Proposal } from '..';
+import styles from '../../Table/styles.module.scss';
 
 function ProposalTable({ proposals, controls }) {
   const proposal = proposals[0];
@@ -9,77 +10,95 @@ function ProposalTable({ proposals, controls }) {
     section,
   } = proposal.toHuman();
 
-  const headings = React.useMemo(() => {
+  const { headings, small } = React.useMemo(() => {
     if (method === 'transfer' && section === 'balances') {
-      return [
-        'Transfer',
-        'To',
-      ];
+      return {
+        headings: [
+          'Transfer',
+          'To',
+        ],
+        small: true,
+      };
     }
     if ((method === 'sendLlmToPolitipool' || method === 'sendLlm') && section === 'llm') {
-      return [
-        'Transfer',
-        'To',
-      ];
+      return {
+        headings: [
+          'Transfer',
+          'To',
+        ],
+        small: true,
+      };
     }
     if (method === 'transfer' && section === 'assets') {
-      return [
-        'Transfer',
-        'To',
-      ];
+      return {
+        headings: [
+          'Transfer',
+          'To',
+        ],
+        small: true,
+      };
     }
     if (method === 'remark' && section === 'llm') {
-      return [
-        'Category',
-        'Project',
-        'Supplier',
-        'Description',
-        'Currency',
-        'Amount in USD',
-        'Final Destination',
-        'Date',
-      ];
+      return {
+        headings: [
+          'Category',
+          'Project',
+          'Supplier',
+          'Description',
+          'Currency',
+          'Amount in USD',
+          'Final Destination',
+          'Date',
+        ],
+        small: false,
+      };
     }
     // eslint-disable-next-line no-console
     console.warn(`Trying to display proposal ${method}/${section} as table. Unsupported`);
-    return [];
+    return { headings: [], small: true };
   }, [method, section]);
 
+  const hasControls = React.useMemo(() => controls?.some(Boolean), [controls]);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          {headings.map((heading) => (
-            <th key={heading}>
-              {heading}
-            </th>
-          ))}
-          {controls && (
-            <th>
-              Controls
-            </th>
-          )}
-        </tr>
-      </thead>
-      <tbody>
-        {proposals.map((prop, index) => (
-          <tr key={prop}>
-            <Proposal proposal={prop} isTableRow />
-            {controls && (
-              <td>
-                {controls[index]}
-              </td>
-            )}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className={styles.tableScrollContainer}>
+      <div className={small ? styles.tableScrollSmallInnerContainer : styles.tableScrollBigInnerContainer}>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              {headings.map((heading) => (
+                <th key={heading}>
+                  {heading}
+                </th>
+              ))}
+              {hasControls && (
+                <th>
+                  Controls
+                </th>
+              )}
+            </tr>
+          </thead>
+          <tbody className={styles.tbody}>
+            {proposals.map((prop, index) => (
+              <tr key={prop}>
+                <Proposal proposal={prop} isTableRow />
+                {hasControls && (
+                  <td>
+                    {controls[index]}
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
 ProposalTable.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  proposals: PropTypes.shape(PropTypes.object.isRequired).isRequired,
+  proposals: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
   controls: PropTypes.arrayOf(PropTypes.node),
 };
 
