ea5923640884b0f3095fa588e440df2f3d11cdb0
jindra12.underdark <jindra12.underdark@gmail.com>
2024-11-15 21:59:02 +0100

    BLOCKCHAIN-542 remove useless extra tabs

--- src/components/Contracts/index.js
+++ src/components/Contracts/index.js
@@ -1,40 +1,31 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import stylesPage from '../../utils/pagesBase.module.scss';
 import router from '../../router';
 import HomeContract from './Home';
 import Contract from './Contract';
 import MyContracts from './MyContracts';
-import RegistriesHeader from './ContractsHeader';
 
 export default function Contracts() {
   return (
-    <div className={stylesPage.sectionWrapper}>
-      <div className={stylesPage.menuAddressWrapper}>
-        <RegistriesHeader />
-      </div>
-      <div className={stylesPage.contentWrapper}>
-        <Switch>
-          <Route
-            exact
-            path={router.contracts.overview}
-            component={HomeContract}
-          />
-          <Route
-            exact
-            path={router.contracts.myContracts}
-            component={MyContracts}
-          />
-          <Route path={router.contracts.item} component={Contract} />
-          <Route
-            exact
-            path={router.home.contracts}
-            render={() => (
-              <Redirect to={router.contracts.overview} />
-            )}
-          />
-        </Switch>
-      </div>
-    </div>
+    <Switch>
+      <Route
+        exact
+        path={router.contracts.overview}
+        component={HomeContract}
+      />
+      <Route
+        exact
+        path={router.contracts.myContracts}
+        component={MyContracts}
+      />
+      <Route path={router.contracts.item} component={Contract} />
+      <Route
+        exact
+        path={router.home.contracts}
+        render={() => (
+          <Redirect to={router.contracts.overview} />
+        )}
+      />
+    </Switch>
   );
 }
--- src/components/Registries/index.js
+++ src/components/Registries/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import router from '../../router';
-import stylesPage from '../../utils/pagesBase.module.scss';
 
-import RegistriesHeader from './RegistriesHeader';
 import RegistriesOverview from './RegistriesOverview';
 import RegistriesLand from './RegistriesLand';
 import RegistriesAssets from './RegistriesAssets';
@@ -11,42 +9,35 @@ import RegistriesOther from './RegistriesOther';
 
 function Registries() {
   return (
-    <div className={stylesPage.sectionWrapper}>
-      <div className={stylesPage.menuAddressWrapper}>
-        <RegistriesHeader />
-      </div>
-      <div className={stylesPage.contentWrapper}>
-        <Switch>
-          <Route
-            exact
-            path={router.registries.overview}
-            component={RegistriesOverview}
-          />
-          <Route
-            exact
-            path={router.home.registries}
-            render={() => (
-              <Redirect to={router.registries.overview} />
-            )}
-          />
-          <Route
-            exact
-            path={router.registries.land}
-            component={RegistriesLand}
-          />
-          <Route
-            exact
-            path={router.registries.assets}
-            component={RegistriesAssets}
-          />
-          <Route
-            exact
-            path={router.registries.other}
-            component={RegistriesOther}
-          />
-        </Switch>
-      </div>
-    </div>
+    <Switch>
+      <Route
+        exact
+        path={router.registries.overview}
+        component={RegistriesOverview}
+      />
+      <Route
+        exact
+        path={router.home.registries}
+        render={() => (
+          <Redirect to={router.registries.overview} />
+        )}
+      />
+      <Route
+        exact
+        path={router.registries.land}
+        component={RegistriesLand}
+      />
+      <Route
+        exact
+        path={router.registries.assets}
+        component={RegistriesAssets}
+      />
+      <Route
+        exact
+        path={router.registries.other}
+        component={RegistriesOther}
+      />
+    </Switch>
   );
 }
 
