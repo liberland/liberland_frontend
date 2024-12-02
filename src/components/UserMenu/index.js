c44530b5299871e7a761b88c3e21ec7e5e6541f0
jindra12.underdark <jindra12.underdark@gmail.com>
2024-12-01 22:35:13 -0600

    BLOCKCHAIN-542 Add e-residency modal

--- src/components/UserMenu/index.js
+++ src/components/UserMenu/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import DownOutlined from '@ant-design/icons/DownOutlined';
 import { useHistory } from 'react-router-dom';
@@ -11,8 +11,8 @@ import UserIcon from '../../assets/icons/user.svg';
 import { userSelectors } from '../../redux/selectors';
 import { authActions } from '../../redux/actions';
 import ChangeWallet from '../Home/ChangeWallet';
+import { GetCitizenshipModal } from '../Modals';
 import styles from './styles.module.scss';
-import GetCitizenshipCard from '../Home/Cards/GetCitizenshipCard';
 
 function UserMenu({
   isEResident,
@@ -22,6 +22,7 @@ function UserMenu({
   const user = useSelector(userSelectors.selectUser);
   const dispatch = useDispatch();
   const isBiggerThanSmallScreen = useMediaQuery('(min-width: 576px)');
+  const [isCitizenshipModalOpen, setCitizenshipModalOpen] = useState();
 
   const logAction = user ? {
     key: 'logout',
@@ -33,37 +34,44 @@ function UserMenu({
 
   const eResidentAction = {
     key: 'eresident',
-    label: <GetCitizenshipCard />,
+    label: 'Get a Liberland Citizenship!',
   };
 
   return (
-    <Dropdown
-      menu={{
-        items: [
-          logAction,
-        ].concat(isBiggerThanSmallScreen ? [] : [{
-          key: 'wallets',
-          label: <ChangeWallet />,
-        }]).concat(isEResident ? [eResidentAction] : []),
-        onClick: ({ key }) => {
-          if (key === 'login') {
-            login();
-          } else if (key === 'logout') {
-            logOut();
-            dispatch(authActions.signOut.call(history));
-            window.location.href = `${
-              process.env.REACT_APP_SSO_API}/logout?redirect=${process.env.REACT_APP_FRONTEND_REDIRECT}`;
-          }
-        },
-      }}
-      trigger={['click']}
-    >
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#" onClick={(e) => e.preventDefault()} className={styles.dropdownLink}>
-        <Avatar src={UserIcon} size={30} className={styles.avatar} />
-        <DownOutlined />
-      </a>
-    </Dropdown>
+    <>
+      <Dropdown
+        menu={{
+          items: [
+            logAction,
+          ].concat(isBiggerThanSmallScreen ? [] : [{
+            key: 'wallets',
+            label: <ChangeWallet />,
+          }]).concat(isEResident ? [eResidentAction] : []),
+          onClick: ({ key }) => {
+            if (key === 'login') {
+              login();
+            } else if (key === 'logout') {
+              logOut();
+              dispatch(authActions.signOut.call(history));
+              window.location.href = `${
+                process.env.REACT_APP_SSO_API}/logout?redirect=${process.env.REACT_APP_FRONTEND_REDIRECT}`;
+            } else if (key === 'eresident') {
+              setCitizenshipModalOpen(true);
+            }
+          },
+        }}
+        trigger={['click']}
+      >
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a href="#" onClick={(e) => e.preventDefault()} className={styles.dropdownLink}>
+          <Avatar src={UserIcon} size={30} className={styles.avatar} />
+          <DownOutlined />
+        </a>
+      </Dropdown>
+      {isCitizenshipModalOpen && (
+        <GetCitizenshipModal closeModal={() => setCitizenshipModalOpen(false)} />
+      )}
+    </>
   );
 }
 
