ea2f37df656cc2b63c4bb9b7c66e01509f74c11f
jindra12.underdark <jindra12.underdark@gmail.com>
2024-12-23 15:22:37 -0600

    BLOCKCHAIN-549 Add caching to loader

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,9 +1,20 @@
 import React, { lazy, Suspense } from 'react';
 import Spin from 'antd/es/spin';
 
+const cache = {};
+
 export function loader(imported) {
-  const Component = lazy(imported);
+  const key = imported.toString();
+  const Component = lazy(async () => {
+    const loaded = await imported();
+    cache[key] = loaded.default;
+    return loaded;
+  });
   return function render() {
+    const Cached = cache[key];
+    if (Cached) {
+      return <Cached />;
+    }
     return (
       <Suspense fallback={<Spin />}>
         <Component />
