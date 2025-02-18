3d7179a80afbce577455b9d97ea3ba55cecec50b
Szymon.Zieba <szymon.zieba@neti-soft.com>
2025-02-18 21:27:42 +0100

    BLOCKCHAIN-594 - FE and middleware add financial metrics

--- src/components/Offices/Finances/index.js
+++ src/components/Offices/Finances/index.js
@@ -10,7 +10,7 @@ import { useMediaQuery } from 'usehooks-ts';
 import { officesActions, financesActions } from '../../../redux/actions';
 import { officesSelectors, financesSelectors } from '../../../redux/selectors';
 import Table from '../../Table';
-import { formatDollars, formatMerits } from '../../../utils/walletHelpers';
+import { formatCustom, formatDollars, formatMerits } from '../../../utils/walletHelpers';
 import { palletIdToAddress } from '../../../utils/pallet';
 import CurrencyIcon from '../../CurrencyIcon';
 import CopyIconWithAddress from '../../CopyIconWithAddress';
@@ -233,6 +233,15 @@ export default function Finances() {
                 {
                   metric: 'Staker APY',
                   value: formatPercent(finances.stakerApyWeeklyPayouts),
+                },
+                {
+                  metric: 'Total LLD',
+                  value: (
+                    <Flex wrap gap="10px" align="center">
+                      {formatCustom(finances.totalLld, 0)}
+                      <CurrencyIcon size={20} symbol="LLD" />
+                    </Flex>
+                  ),
                 },
               ]}
               noPagination
