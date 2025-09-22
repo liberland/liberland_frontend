eb6ff49b5d6898e32a41dc7095cd7ef87271ec13
jindra12 <jindra12.underdark@gmail.com>
2025-09-22 23:35:40 +0200

    Remove console.log

--- src/components/Legislation/LegislationView/AmendButton/index.js
+++ src/components/Legislation/LegislationView/AmendButton/index.js
@@ -11,7 +11,6 @@ import CongressAmendLegislationViaReferendumModal from '../../../Modals/Congress
 function AmendButton({
   tier, id, section,
 }) {
-  console.log({ tier, id, section });
   return typeof section === 'number' ? (
     <Dropdown
       trigger={['click']}
